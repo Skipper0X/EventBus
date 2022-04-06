453f7b9e78669a60cba70ce978ce77a892862c67
Skipper <88627341+Skipper0X@users.noreply.github.com>
2022-04-06 16:34:42 +0500

    - demo mono behaviour for EventBus use case

---
+++ EventBusDemoBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using Core.Runtime;
+using UnityEngine;
+
+public readonly struct InfoEvent : IEvent
+{
+	public readonly string Info;
+	public InfoEvent(string info) => Info = info;
+}
+
+public class EventBusDemoBehaviour : MonoBehaviour
+{
+	private void OnEnable() => EventBus.Subscribe<InfoEvent>(OnInfoEvent);
+	private void OnDisable() => EventBus.UnSubscribe<InfoEvent>(OnInfoEvent);
+	private void OnInfoEvent(InfoEvent infoEvent) => Debug.Log(infoEvent.Info, this);
+
+
+	[ContextMenu(nameof(PublishInfoEvent))]
+	private void PublishInfoEvent()
+	{
+		EventBus.Publish<InfoEvent>(new InfoEvent("InfoEvent: DemoRun..."));
+	}
+}
